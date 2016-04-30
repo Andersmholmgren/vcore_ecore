52608a91e0d9a237975e708b9ea7e7f6f1c392d0
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-30 17:35:15 +1000

    vcore model generator

--- lib/src/model/ecore.g.dart
+++ lib/src/model/ecore.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// 2016-04-30T07:03:31.418608Z
+// 2016-04-30T07:34:35.218703Z
 
 part of ecore;
 
@@ -2298,13 +2298,6 @@ class _$EInvocationTargetExceptionSerializer
     return result.build();
   }
 }
-
-// **************************************************************************
-// Generator: VCoreModelGenerator
-// Target: library ecore
-// **************************************************************************
-
-// Error: Bad state: failed to resolve classifier helper class: Object
 
 // **************************************************************************
 // Generator: BuiltValueGenerator
---
+++ lib/src/model/vcore_models.dart
@@ -0,0 +1,8 @@
+library vCoreModelPackage;
+
+import 'ecore.dart';
+import 'package:vcore/vcore.dart';
+
+part 'vcore_models.g.dart';
+
+Package vCoreModelPackage = _$vCoreModelPackage;
