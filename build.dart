bef20d4332543040a272fa923c4da3123385a690
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-30 17:03:06 +1000

    vcore model generator

--- build.dart
+++ build.dart
@@ -2,12 +2,17 @@ import 'package:built_value_generator/built_value_generator.dart';
 import 'package:built_json_generator/built_json_generator.dart';
 
 import 'package:source_gen/source_gen.dart';
+import 'package:vcore_generator/vcore_generator.dart';
 
 /// Example of how to use source_gen with [BuiltValueGenerator].
 ///
 /// All you need is to import the generators you want and call [build].
 void main(List<String> args) {
-  build(args, [new BuiltJsonGenerator(), new BuiltValueGenerator()],
-    librarySearchPaths: ['lib/src/model'])
-    .then((result) => print(result));
+  build(args, [
+    new BuiltJsonGenerator(),
+    new BuiltValueGenerator(),
+    new VCoreModelGenerator()
+  ], librarySearchPaths: [
+    'lib/src/model'
+  ]).then((result) => print(result));
 }
