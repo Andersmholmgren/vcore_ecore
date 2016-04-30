9ff47dd8c915e6b402b2bda08c187b113dfb057d
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-30 17:46:11 +1000

    vcore model generator

---
+++ lib/src/model/serializers.dart
@@ -0,0 +1,14 @@
+library serializers;
+
+import 'package:built_collection/built_collection.dart';
+import 'package:built_json/built_json.dart';
+import 'package:vcore/src/model/model.dart';
+
+part 'serializers.g.dart';
+
+/// Example of how to use built_json.
+///
+/// Declare a top level [Serializers] field called
+/// serializers. The built_json code generator will provide the
+/// implementation. You usually only need to do this once per project.
+Serializers serializers = _$serializers;
